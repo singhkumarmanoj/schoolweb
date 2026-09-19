5bc5a7d244c9555a3d0d7db6c2e6c45db3cac61c
manoj <sm6361098@gmail.com>
2026-09-19 18:14:16 +0545

    group image

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,28 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="dark:bg-black flex w-full">
+    <div className="dark:bg-black w-full">
       <div className="bg-blue-600 w-full flex justify-center">
         <Image className="rounded-full mr-1"
-          src="/image1.png"
+          src="/school/image1.png"
           alt="logo of school"
           width={60}
           height={20} />
         <h1 className="text-2xl text-gray-50 font-serif  max-w-70 flex-1 self-center justify-center pl-2 pr-2">Janta Higher Secondary Shool Shree Nager</h1>
       </div>
-      
+
+      <div className="border-2 border-amber-200 rounded-2xl m-1">
+        <Image 
+          className="w-full rounded-2xl "
+        
+          src="/school/group.jpg"
+          alt="group image of our teachers"
+          width={300}
+          height={200}
+          
+          
+          />
+      </div>
   
     </div>
   );
