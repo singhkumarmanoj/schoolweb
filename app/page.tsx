ce4317c08d670cf2d49bafeb222b71a6727509b4
manoj <sm6361098@gmail.com>
2026-09-19 14:45:16 +0545

    logo

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Janta Higher Secondary Shool Shree Nager</h1>
+    <div className="dark:bg-black flex w-full">
+      <div className="bg-blue-600 w-full flex justify-center">
+        <Image className="rounded-full mr-1"
+          src="/image1.png"
+          alt="logo of school"
+          width={60}
+          height={20} />
+        <h1 className="text-2xl font-serif bg-amber-500 max-w-70 flex-1 self-center justify-center pl-2 pr-2">Janta Higher Secondary Shool Shree Nager</h1>
+      </div>
+      
   
     </div>
   );
