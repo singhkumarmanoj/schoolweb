f6f1f5f3d2220dbe174ce60a7d37a104e48efd47
manoj <sm6361098@gmail.com>
2026-09-19 19:56:32 +0545

    update name

--- app/ImagePart/page.tsx
+++ app/ImagePart/page.tsx
@@ -4,18 +4,20 @@ import Image from "next/image";
 
 export default function ImagePart(){
     return(
-        <div>
-            <h1 className=" flex items-center">Additional Images</h1>
+        <div className="dark:bg-green-950">
+            <h1 className=" flex justify-center items-center text-2xl font-mono">Additional Images</h1>
 
-            <div>
+            <div className="flex flex-col gap-2">
+              
               <Image
-                className="w-full rounded-2xl border-2 border-amber-300 mb-3"
+                className=" w-full rounded-2xl border-2 border-amber-300 mb-3"
                 src="/school/Screenshot 2026-09-19 103622.png"
                 alt="school"
                 width={300}
                 height={200}
                 
                 />
+
               <Image
                 className="w-full rounded-2xl border-2 border-amber-300 mb-3"
                 src="/school/Screenshot 2026-09-19 101959.png"
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="dark:bg-black w-full">
+    <div className="dark:bg-green-950 w-full">
       <div className="bg-blue-600 w-full flex justify-center">
-        <Image className="rounded-full mr-1"
-          src="/school/image1.png"
-          alt="logo of school"
-          width={60}
-          height={20} />
+        <a href="/">
+          <Image className="rounded-full mr-1"
+            src="/school/image1.png"
+            alt="logo of school"
+            width={60}
+            height={20} 
+          />
+        </a>
+        <a href="/">
+
         <h1 className="text-2xl text-gray-50 font-serif  max-w-70 flex-1 self-center justify-center pl-2 pr-2">Janta Higher Secondary Shool Shree Nager</h1>
+        </a>
       </div>
 
       <div className="border-2 border-amber-200 rounded-2xl m-1">
@@ -26,20 +32,20 @@ export default function Home() {
       </div>
 
 
-      <div>
-        <a className="text-blue-600" href="/ImagePart">Click for more Images</a>
+      <div className="mt-2 ml-3">
+        <a className=" text-blue-300 hover:text-blue-500  font-serif hover:underline decoration-dotted" href="/ImagePart">Click for more Images</a>
       </div>
 
 
       <div className="mt-5">
         <h1 className="text-2xl font-bold">Contact Us</h1>
-        <a className="ml-3 text-sm text-blue-200 hover:text-blue-500 hover:underline decoration-dotted" href="https://www.google.com/maps/place/Janta+Higher+Secondary+School+Shree+Nagar/@27.4384984,84.8520901,291351m/data=!3m1!1e3!4m6!3m5!1s0x39ec7b3a29876849:0xa4674e25cfd386b1!8m2!3d26.9686548!4d85.7094581!16s%2Fg%2F11rcwxhdt5?entry=ttu&g_ep=EgoyMDI2MDkxNi4wIKXMDSoASAFQAw%3D%3D">Visit in map</a>
+        <a className="font-serif ml-3 text-sm text-blue-300 hover:text-blue-500 hover:underline decoration-dotted" href="https://www.google.com/maps/place/Janta+Higher+Secondary+School+Shree+Nagar/@27.4384984,84.8520901,291351m/data=!3m1!1e3!4m6!3m5!1s0x39ec7b3a29876849:0xa4674e25cfd386b1!8m2!3d26.9686548!4d85.7094581!16s%2Fg%2F11rcwxhdt5?entry=ttu&g_ep=EgoyMDI2MDkxNi4wIKXMDSoASAFQAw%3D%3D">Visit in map</a>
       </div>
 
-      <div className="bg-gray-500 max-w-60 rounded-2xl p-1 flex flex-col items-center justify-center mt-10 mb-5 ml-3">
-        <p className="text-lg">Add more info then mail me
+      <div className="bg-gray-700 max-w-60 rounded-2xl p-1 flex flex-col items-center justify-center mt-10 mb-5 ml-3">
+        <p className="text-lg text-black">Add more info then mail me
         </p>
-        <p className="text-sm">sm6361098@gmail.com</p>
+        <p className="text-sm text-black">sm6361098@gmail.com</p>
 
       </div>
 
